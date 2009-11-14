52badfe39896a646a2d73420144209ad4a7825f4
Daniel Lukic <daniel.lukic@berlinfactor.com>
2009-11-15 00:09:59 +0100

    Removed some config examples to avoid Groovy compilation problems.

--- config/Ruby/Syntax.groovy
+++
@@ -1,8 +0,0 @@
-
-package Ruby
-
-def syntax = new File( configFolder, 'Ruby/Syntax.flux' ).text
-def code = new flux.FluxBuilder( configFolder ).using( syntax ).create( "RubySyntaxFlexer" )
-def clazz = evaluate( code )
-def instance = clazz.newInstance( configuration )
-return new com.intellij.lexer.FlexAdapter( instance )
